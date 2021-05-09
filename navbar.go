0434fa67aeccb606b62ea4d8fcf668fc2c9b9247
akakou <akakou571@gmail.com>
2021-05-09 13:40:05 +0900

    [add] File manager

--- navbar.go
+++ navbar.go
@@ -3,6 +3,7 @@ package uree_core_package
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	uree_package "github.com/akakou-hobby/uree-package"
 	"github.com/sqweek/dialog"
@@ -43,4 +44,34 @@ func (pkg CommpileCPackage) SetUpOptional() string {
 
 func (pkg CommpileCPackage) GetName() string {
 	return "Run C"
+}
+
+type FileSidePallet struct {
+}
+
+func (pkg FileSidePallet) Run(req uree_package.Request) uree_package.Response {
+	divided := strings.Split(req.Path, "/")
+	divided = divided[:len(divided)-1]
+
+	parent := strings.Join(divided, "/")
+	fmt.Print(parent)
+
+	out, _ := exec.Command("tree", parent).Output()
+
+	filtered_out := strings.Replace(string(out[:10000]), " ", "&ensp;", -1)
+	filtered_out = strings.Replace(filtered_out, "\n", "<br>", -1)
+
+	return uree_package.Response{filtered_out}
+}
+
+func (pkg FileSidePallet) SetUpOptional() string {
+	return ""
+}
+
+func (pkg FileSidePallet) GetName() string {
+	return ""
+}
+
+func (pkg FileSidePallet) GetIconPath() string {
+	return "./img/file.png"
 }
